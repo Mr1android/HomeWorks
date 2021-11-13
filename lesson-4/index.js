c7f8e5842fe05a7c1d40963a9dc557545b57afa1
Mr1android <umarmedilkanovumar@gmail.com>
2021-11-13 20:19:51 +0600

    first commit

---
+++ lesson-4/index.js
@@ -0,0 +1,24 @@
+// const input = document.getElementById('task')
+// console.log(input)
+
+//
+// const blocks = document.getElementsByClassName('task')
+// console.log(blocks)
+//
+
+// input.onchenge = (e)=> {
+//     console.log(e)
+// }
+
+
+const  input= document.getElementById('task')
+const  input= document.querySelector('.save_task')
+
+//
+// button.onclick = () => {
+//     console.log(input.value)
+}
+const getValue = (e) = {
+    e.target.classList.toggle('active')
+}
+
